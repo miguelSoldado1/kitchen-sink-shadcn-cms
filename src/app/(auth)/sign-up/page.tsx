50b1e78a8fa70c627524a8206cd6a6849303a9aa
MIGUEL SOLDADO <miguel-a-soldado@telecom.pt>
2025-08-05 10:07:55 +0100

    added sign up form + useful scripts to package.json

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,7 +1,11 @@
+import { SignUpForm } from "./sign-up-form";
+
 export default function SignUpPage() {
   return (
     <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="w-full max-w-sm"></div>
+      <div className="w-full max-w-sm">
+        <SignUpForm />
+      </div>
     </div>
   );
 }
